8f5ab73dc26c2d15c97e6081b74cd35eb1c8ee16
Vignesh SB <92362676+vigneshsb403@users.noreply.github.com>
2023-08-23 16:42:56 +0530

    Create index.php

---
+++ htdocs/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            It works.
+        </title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+    <p1>The site is up &#10084;&#65039;&#8205;&#128293;.</p1>
+    </body>
+    <header>
+        <h1>Welcome to My Simple Site</h1>
+    </header>
+    <main>
+        <p>This is some content on the main page.</p>
+    </main>
+    <script src="js/app.o.js"></script> 
+</html>
